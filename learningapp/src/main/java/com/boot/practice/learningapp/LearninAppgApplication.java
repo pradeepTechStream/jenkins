f0cf14cd7ac9d7a87ac2e65cf205099c5d0e29e0
PM2491 <pradeep.maurya@sourceably.com>
2022-10-20 22:27:00 +0530

    initial project

---
+++ learningapp/src/main/java/com/boot/practice/learningapp/LearninAppgApplication.java
@@ -0,0 +1,13 @@
+package com.boot.practice.learningapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearninAppgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearninAppgApplication.class, args);
+	}
+
+}
---
+++ learningapp/src/test/java/com/boot/practice/learningapp/LearninAppgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boot.practice.learningapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearninAppgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
